705e490f7273cc8f2b0c4af4c78217f4a9720349
Sergio Angulo Martín <sangulo@paradigmadigital.com>
2023-01-09 09:58:40 +0100

    basic router

---
+++ controllers/static.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func StaticHandlerJSON(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	resp := make(map[string]string)
+	resp["message"] = "Working"
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Write(jsonResp)
+}
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"musenaw/go-brawl-api/controllers"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/", controllers.StaticHandlerJSON)
+
+	r.Post("/signup", controllers.StaticHandlerJSON)
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", r)
+}
